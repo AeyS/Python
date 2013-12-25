1c55e3ef75cded300e7993c8a8312a063486e5a7
AeyS <leefanquan@hotmail.com>
2013-12-25 17:58:01 +0800

    Update Average_sorting.py

--- Average_sorting.py
+++ Average_sorting.py
@@ -2,20 +2,13 @@
 import rw
 
 success_list=[] #Meet the requirements of the combined group
-max_min=[]  #Max ad min volue save var;[function_name : max_min_mark]
-
-def count(x,y):
-    result=x+y
-    return result
-
-def count_list(x,y):
-    total=count(len(x),len(y))
-    return total
     
 def max_min_mark(var):
+    max_min=[]  #Max ad min volue save var;[function_name : max_min_mark]
     for i in var:
         length=len(i)
         max_min.append(length)
+    return max_min
 
 
 def merger_group(textdir):
@@ -23,7 +16,7 @@ def merger_group(textdir):
     b_split=[]
     for i in xrange(0,len(textlines)):
         if i%2!=0:
-            if count_list(x, textlines[i])>35:
+            if len(x)+len(textlines[i])>35:
                 b_split.append(x)
                 b_split.append(textlines[i])
             else:
@@ -33,7 +26,7 @@ def merger_group(textdir):
     return b_split
 
 def best_value(b_split):
-    max_min_mark(b_split)
+    max_min=max_min_mark(b_split)
     min_value_location=max_min.index(min(max_min))
     while min_value_location:
         max_value_location=max_min.index(max(max_min))
